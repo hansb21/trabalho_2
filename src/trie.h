065f71f7d41fdb4cce3b5fad9a28d580071010c6
Andre Fernandes <fpf.andre@gmail.com>
2020-12-02 23:54:38 -0300

    add "find" and "countSufixes" functions on trie

--- src/trie.h
+++ src/trie.h
@@ -12,8 +12,8 @@ namespace structures {
 	struct NoTrie* initi();
 
 	void insert(struct NoTrie* root, std::string key, unsigned long pos, unsigned long size);
-	
-	std::pair<int, int> find(struct NoTrie*, std::string word);
+
+	// std::pair<int, int> find(struct NoTrie*, std::string word);
 
 #include "trie.inc"
 
