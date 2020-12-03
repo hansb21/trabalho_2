913e481537caed50a135cad51b832b3a193893c8
Andre Fernandes <fpf.andre@gmail.com>
2020-12-02 23:55:15 -0300

    create dic.h file and show results on main.cpp

---
+++ src/dic.h
@@ -0,0 +1,13 @@
+#include "trie.h"
+
+using namespace std;
+using namespace structures;
+
+NoTrie* parseDict(std::string filename) {
+	NoTrie* root = init();
+	string word;
+
+    // confia
+
+    return root;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,42 @@
 #include <string>
 
 #include "trie.h"
+#include "dic.h"
+
+void outputResults(structures::NoTrie* node, std::string word) {
+    int sufixes = structures::countSufixes(node);
+    int pos = node->pos;
+    int size = node->size;
+
+    if (sufixes == 0) {
+        std::cout << word << " is not prefix" << std::endl;
+    } else {
+        std::cout << word << " is prefix of " << sufixes << " words" << std::endl;
+    }
+
+    if (size > 0) {
+        std::cout << word << " is at (" << pos << " " << size << ")" << std::endl;
+    }
+}
 
 int main() {
     
     using namespace std;
+    using namespace structures;
 
     string filename;
     string word;
 
     cin >> filename;  // entrada
+    NoTrie* root = parseDict(filename);
 
-    while (1) {  // leitura das palavras ate' encontrar "0"
+    // leitura das palavras até encontrar "0"
+    while (true) {
         cin >> word;
-        if (word.compare("0") == 0) {
-            break;
-        }
-        cout << word << endl;
+        if (word.compare("0") == 0) break;
+
+        NoTrie* node = find(root, word);
+        outputResults(node, word);
     }
 
     return 0;
