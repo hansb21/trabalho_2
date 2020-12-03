32309cb7b3bd60b116dae84c2f5349ffe596cfee
Hans Buss <hans.buss@mailfence.com>
2020-12-03 17:41:07 -0300

    confia

--- src/dic.h
+++ src/dic.h
@@ -3,10 +3,42 @@
 using namespace std;
 using namespace structures;
 
-NoTrie* parseDict(std::string filename) {
-	NoTrie* root = init();
-	string word;
+int nextPos = 0;
 
+NoTrie* parseDict(std::string filename) {
+	
+	NoTrie* root = init();
+	string line;
+	ifstream file(filename);
+	if (file.is_open()) 
+	{
+		while(getline(file, line))
+		{	
+			int i;
+			string word;
+			if (line[0] != '[')
+			{
+				nextPos += line.length() +1;
+				continue;
+			}
+			for (i = 1; i < line.length(); i++)
+			{
+				if(line[i] == ']')
+					break;
+				
+				word += line[i];
+			}
+			
+			word[i] = '\0';
+			insert(root, word, nextPos, line.length());
+			nextPos += line.length() + 1;
+		}
+		file.close();
+	} else {
+		printf("nao abriu arquivo\n");
+	
+	
+	}
     // confia
 
     return root;
